1a2988b48e6ea1bc87d52268843082b3afb5cc2e
zainalbani <zainalbani007@gmail.com>
2022-08-27 09:24:33 +0700

    Challenge2

---
+++ app/src/main/java/com/example/androidchallenge2/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.androidchallenge2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var editText : EditText
+    private lateinit var textView : TextView
+    private lateinit var radioAmazing : RadioButton
+    private lateinit var radioGood : RadioButton
+    private lateinit var radioOk : RadioButton
+    private lateinit var switch : Switch
+    private lateinit var btnCalculate : Button
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editText = findViewById(R.id.editText)
+        textView = findViewById((R.id.textView1))
+        radioAmazing = findViewById(R.id.radioAmazing)
+        radioGood = findViewById(R.id.radioGood)
+        radioOk = findViewById(R.id.radioOk)
+        switch = findViewById(R.id.switchTip)
+        btnCalculate = findViewById(R.id.btnCalculate)
+    }
+}
